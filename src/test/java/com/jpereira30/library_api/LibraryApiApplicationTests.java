2453da8b3fd30acf490bbfb3b1a7c3643d561b5a
Joshua Pereira <JPereira@munichre.digital>
2025-02-14 00:08:07 +0000

    Initial commit on dev branch with proper .gitignore

---
+++ src/test/java/com/jpereira30/library_api/LibraryApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jpereira30.library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
